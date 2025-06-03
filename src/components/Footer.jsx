84caa39439014b5be6c713da1fbf2e3cde0c86f5
bryanrios0060 <bryanrios0060@gmail.com>
2025-06-03 20:28:18 +0000

    imagen de mapa

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,13 +14,16 @@ const Footer = () => {
     >
       <div className="container mx-auto px-4 text-center">
         {/* Aquí es donde se coloca el iframe del mapa */}
-        
         <p className="text-sm">
           © {currentYear} UniForum - Corporación Universitaria Lasallista. Todos los derechos reservados.
         </p>
         <p className="text-xs mt-2">
           Una plataforma para la socialización del conocimiento académico.
         </p>
+        <img 
+          src="https://i.imgur.com/sFEwxry.png" 
+          alt="Map of Corporación Universitaria Lasallista" 
+          className="mt-4 mx-auto" />
         <div className="mt-4 space-x-4">
           <a href="#" className="hover:text-primary transition-colors text-sm">Política de Privacidad</a>
           <span className="text-gray-400">|</span>
